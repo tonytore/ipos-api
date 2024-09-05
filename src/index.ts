0e275acf94095b03c5628e12f2a442766a5b65fb
tonytor <teme4842@gmail.com>
2024-09-05 19:12:37 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,10 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.listen(PORT, () => {
-  // Start the server and listen on the specified port
   console.log(`Server is running on http://localhost:${PORT}`); // Log a message indicating the server is running
 });
 
-// Create an API
-// GET, POST,PUT,PATCH,DELETE
-// http:localhost:8000/customers
+
 app.get("/customers", async (req: Request, res: Response) => {
   const customers = [
     { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
